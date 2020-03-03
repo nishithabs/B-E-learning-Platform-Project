7650b37852eb7ac7b2a3c70dad5c0600ecb45a04
Devin Dumais <devin@frameworkhomeownership.org>
2020-03-03 14:25:13 -0500

    add react router and initial routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { Route } from 'react-router-dom'
+
+import routes from './constants/routes';
+
+import About from './containers/About';
+import ContactUs from './containers/ContactUs';
+import Home from './containers/Home';
+
+import Navigation from './components/Navigation'
 
 const App = () => (
   <main>
-    <p>
-      Team B#
-    </p>
-    <p>
-      The Apex of Agility
-    </p>
+    <Navigation />
+
+    <hr />
+
+    <Route path={routes.ABOUT}>
+      <About />
+    </Route>
+    <Route path={routes.CONTACT_US}>
+      <ContactUs />
+    </Route>
+    <Route exact path={routes.HOME}>
+      <Home />
+    </Route>
   </main>
 );
 
---
+++ src/containers/Home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <>
+      <p>
+        Team B#
+      </p>
+      <p>
+        The Apex of Agility
+      </p>
+    </>
+  )
+};
+
+export default Home;
