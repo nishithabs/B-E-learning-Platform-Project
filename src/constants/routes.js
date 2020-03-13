07754e8fa2cc9aea3ecc9752178f65dd9f5f544e
Devin Dumais <devin@frameworkhomeownership.org>
2020-03-13 16:14:18 -0400

    Add placeholder page containers and routes

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -2,10 +2,13 @@ const slug = '/apex-agility';
 
 const routes = {
   ABOUT: `${slug}/about`,
+  ASSESSMENT: `${slug}/assessment`,
   BECOME_A_TRAINER: `${slug}/become-a-trainer`,
+  COACHING: `${slug}/coaching`,
   CONTACT_US: `${slug}/contact-us`,
   COURSES: `${slug}/courses`,
   HOME: `${slug}/`,
+  RESOURCES: `${slug}/resources`,
   TERMS_AND_CONDITIONS: `${slug}/terms-and-conditions`
 };
 
---
+++ src/containers/Resources/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Resources = () => <section>Resources</section>;
+
+export default Resources;
