602cbbf7c8f0b1117688d87baa02dc6c8bb20d0b
Devin Dumais <devin@frameworkhomeownership.org>
2020-03-13 17:11:40 -0400

    Refactor header navigation; add dropdown

--- src/components/GlobalHeader/Navigation/index.js
+++ src/components/GlobalHeader/Navigation/index.js
@@ -1,23 +1,3 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-import routes from '../../../constants/routes';
-
-const Navigation = () => {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <Link to={routes.HOME}>Home</Link>
-        </li>
-        <li>
-          <Link to={routes.ABOUT}>About</Link>
-        </li>
-        <li>
-          <Link to={routes.CONTACT_US}>Contact Us</Link>
-        </li>
-      </ul>
-    </nav>
-)};
+import Navigation from './Navigation';
 
 export default Navigation;
