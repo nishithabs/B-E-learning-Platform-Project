a8728a8e4152b846fda849e63af6f6293662556c
shailesh1210 <shaileshtamrakar@gmail.com>
2020-03-30 15:09:56 -0400

    Added images and logos to Course and Assessment page

--- src/containers/Assessment/Assessment.js
+++ src/containers/Assessment/Assessment.js
@@ -5,6 +5,14 @@ import {Link} from 'react-router-dom';
 import content from './Content';
 import Button from '../../components/Button'
 
+import leadership from './assets/leadership.png';
+import organization from './assets/organization.png';
+import portfolio from './assets/portfolio.png';
+import program from './assets/program.png';
+import individual from './assets/individual.png';
+import team from './assets/team.png';
+
+
 const Assessment = () => {
     return(
         <div className={styles.assessmentContainer}>
@@ -13,13 +21,8 @@ const Assessment = () => {
             </div>
             <div className={styles.bottom}>
                 <div className={styles.bFirst}>
-                    <div className={styles.dummy}>
-                        <div className={styles.contactUsBtn}>
-                            <Link>Dummy</Link>
-                        </div>
-                    </div>
                     <div className={styles.title}>
-                        <h1>Maturity Assessment</h1>
+                        <h2>Maturity Assessment</h2>
                     </div>
                     <div className={styles.contactUsBtn}>
                         <Button>
@@ -28,34 +31,70 @@ const Assessment = () => {
                     </div>
                 </div>
                 <div className={styles.bSecond}>
-                    <h1>Why Assessment?</h1>
+                    <h2>Why Assessment?</h2>
                     <p>{content.placeholder}</p>
                 </div>
                 <div className={styles.bThird}>
-                    <h1> What are the different types of assessment?</h1>
-                    <div>
-                        <h2>Leadership assessment:</h2>
-                        <p>{content.placeholder}</p>
+                    <h2> What are the different types of assessment?</h2>
+                    <div className={styles.assessment}>
+                        <div className={styles.content}>
+                            <h3>Leadership assessment:</h3>
+                            <p>{content.placeholder}</p>
+                        </div>
+                        <div className={styles.logo}>
+                        <img src={leadership} alt={"leadership-logo"}/>
+                        </div>
+                        
                     </div>
-                    <div>
-                        <h2>Organization assessment:</h2>
-                        <p>{content.placeholder}</p>
+                    <div className={styles.assessment}>
+                        <div className={styles.content}>
+                            <h3>Organization assessment:</h3>
+                            <p>{content.placeholder}</p>
+                        </div>
+
+                        <div className={styles.logo}>
+                            <img src={organization} alt={"organization-logo"}/>
+                        </div>   
                     </div>
-                    <div>
-                        <h2>Porfolio assessment:</h2>
-                        <p>{content.placeholder}</p>
+                    <div className={styles.assessment}>
+                        <div className={styles.content}>
+                            <h3>Porfolio assessment:</h3>
+                            <p>{content.placeholder}</p>
+                        </div>
+
+                        <div className={styles.logo}>
+                            <img src={portfolio} alt={"portfolio-logo"}/>
+                        </div>
                     </div>
-                    <div>
-                        <h2>Program assessment:</h2>
-                        <p>{content.placeholder}</p>
+                    <div className={styles.assessment}>
+                        <div className={styles.content}>
+                            <h3>Program assessment:</h3>
+                            <p>{content.placeholder}</p>
+                        </div>
+
+                        <div className={styles.logo}>
+                            <img src={program} alt={"program-logo"}/>
+                        </div>
                     </div>
-                    <div>
-                        <h2>Team assessment:</h2>
-                        <p>{content.placeholder}</p>
+                    <div className={styles.assessment}>
+                        <div className={styles.content}>
+                            <h3>Team assessment:</h3>
+                            <p>{content.placeholder}</p>
+                        </div>
+
+                        <div className={styles.logo}>
+                            <img src={team} alt={"team-logo"}/>
+                        </div>
                     </div>
-                    <div>
-                        <h2>Individual assessment:</h2>
-                        <p>{content.placeholder}</p>
+                    <div className={styles.assessment}>
+                        <div className={styles.content}>
+                            <h3>Individual assessment:</h3>
+                            <p>{content.placeholder}</p>
+                        </div>
+
+                        <div className={styles.logo}>
+                            <img src={individual} alt={"individual-logo"}/>
+                        </div>
                     </div>
                 </div>
 
