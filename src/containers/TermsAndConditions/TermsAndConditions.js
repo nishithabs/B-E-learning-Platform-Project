f50582bcf1e0071076b4b7d9253e0202add9d4ee
Keith Gutfreund <K.Gutfreund@elsevier.com>
2020-04-14 20:07:22 -0400

    Added terms and conditions page

---
+++ src/containers/TermsAndConditions/TermsAndConditions.js
@@ -0,0 +1,150 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import styles from './TermsAndConditions.module.css';
+
+
+const TermsAndConditions = () => {
+  return (
+	  <section> 
+                <div className={styles.termsContainer}>
+	  <h1>Terms And Conditions</h1>
+
+These terms and conditions will explain how Apex uses the personal data we collect from you when you use our website.
+<br/><br/>
+<h2>Topics:</h2>
+<li>
+    What data do we collect?
+</li><li>
+    How do we collect your data?
+</li><li>
+    How will we use your data?
+</li><li>
+    How do we store your data?
+</li><li>
+    Marketing
+</li><li>
+    What are your data protection rights?
+</li><li>
+    What are cookies?
+</li><li>
+    How do we use cookies?
+</li><li>
+    What types of cookies do we use?
+</li><li>
+    How to manage your cookies
+</li><li>
+    Privacy policies of other websites
+</li><li>
+    Changes to our privacy policy
+</li><li>
+      How to contact us
+</li>      
+<br/>
+<h2>What data do we collect?</h2>
+Apex collects the following data:
+<li>
+    Personal identification information (Name, email address, phone number, etc.)
+</li><li>
+    Credit card information is passed on to the credit card processor and is not retained.
+</li>
+	  <br/>
+<h2>	  How do we collect your data?</h2>
+You directly provide Apex with most of the data we collect. We collect data and process data when you:
+<li>
+    Register online or place an order for any of our products or services.
+</li><li>
+    Voluntarily complete a customer survey or provide feedback on any of our message boards or via email.
+</li><li>
+    Use or view our website via your browser’s cookies.
+	  </li>
+<br/>	  
+<h2>How will we use your data?</h2>
+
+Apex collects your data so that we can:
+<li>
+    Process your order and manage your account.
+</li><li>
+    Email you with special offers on other products and services we think you might like.
+	  </li>
+<br/>	  
+If you agree, Apex will share your data with our partner companies so that they may offer you their products and services.
+<br/>	  <br/>
+	  <h2>How do we store your data?</h2>
+	  Apex securely stores your data in the Amazon Cloud, on secured and encrypted servers.
+	  <br/><br/>Apex will keep your data for one year of inactivity. Once this time period has expired, we will remove your data from our servers.
+<br/>	  <br/>
+	  <h2>Marketing</h2>
+Apex would like to send you information about products and services of ours that we think you might like, as well as those of our partner companies.
+If you have agreed to receive marketing, you may always opt out at a later date.
+<br/><br/>
+You have the right at any time to stop Apex from contacting you for marketing purposes or giving your data to other companies.
+<br/>	  <br/>
+	  <h2>What are your data protection rights?</h2>
+Apex would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to the following:
+<br/><br/>
+<li><b>The right to access</b> – You have the right to request Apex for copies of your personal data. We may charge you a small fee for this service.
+</li><li>
+<b>The right to rectification</b> – You have the right to request that Apex correct any information you believe is inaccurate. You also have the right to request Apex to complete the information you believe is incomplete.
+</li><li>
+<b>The right to erasure</b> – You have the right to request that Apex erase your personal data, under certain conditions.
+</li><li>
+<b>The right to restrict processing</b> – You have the right to request that Apex restrict the processing of your personal data, under certain conditions.
+</li><li>
+<b>The right to object to processing</b> – You have the right to object to Apex\'s processing of your personal data, under certain conditions.
+</li><li>
+<b>The right to data portability</b> – You have the right to request that Apex transfer the data that we have collected to another organization, or directly to you, under certain conditions.
+	  </li>
+	  <br/>
+If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us at our email: info@apexagility.com
+	  <br/><br/>
+	  <h2>Cookies</h2>
+
+Cookies are text files placed on your computer to collect standard Internet log information and visitor behavior information. When you visit our websites, we may collect information from you automatically through cookies or similar technology
+<br/><br/>
+For further information, visit allaboutcookies.org.
+	 <br/> <br/>
+	  <h2>How do we use cookies?</h2>
+
+Apex uses cookies in a range of ways to improve your experience on our website, including:
+<li>
+    Keeping you signed in
+</li><li>
+    Understanding how you use our website
+      </li>
+	  <br/>
+<h2>What types of cookies do we use?</h2>
+
+There are a number of different types of cookies, however, our website uses:
+	  <br/>
+<li>	  
+    Functionality – Apex uses these cookies so that we recognize you on our website and remember your previously selected preferences. These could include what language you prefer and location you are in. A mix of first-party and third-party cookies are used.
+</li><li>
+    Advertising – Apex uses these cookies to collect information about your visit to our website, the content you viewed, the links you followed and information about your browser, device, and your IP address. Apex sometimes shares some limited aspects of this data with third parties for advertising purposes. We may also share online data collected through cookies with our advertising partners. This means that when you visit another website, you may be shown advertising based on your browsing patterns on our website.
+	  </li>
+	  <br/>
+	  <h2>
+How to manage cookies</h2>
+
+You can set your browser not to accept cookies, and the above website tells you how to remove cookies from your browser. However, in a few cases, some of our website features may not function as a result.
+<br/>	  <br/>
+	  <h2>Privacy policies of other websites</h2>
+
+The Apex website contains links to other websites. Our privacy policy applies only to our website, so if you click on a link to another website, you should read their privacy policy.
+<br/>	  <br/>
+	  <h2>Changes to our privacy policy</h2>
+
+Apex keeps its privacy policy under regular review and places any updates on this web page. This privacy policy was last updated on 9 January 2019.
+<br/>	  <br/>
+	  <h2>How to contact us</h2>
+
+If you have any questions about Apex’s privacy policy, the data we hold on you, or you would like to exercise one of your data protection rights, please do not hesitate to contact us.
+<br/><br/>
+	  Email us at: info@apexagility.com &nbsp;&nbsp;  This text adapted from: https://gdpr.eu/privacy-notice/
+
+</div>
+    </section>
+  )
+};
+
+export default TermsAndConditions;
